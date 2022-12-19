afc40153ca0e7b3614c58713491f9fa97dccb3ed
mariia.havryliv <mariia.havryliv@gmail.com>
2022-12-19 21:13:32 +0200

    chore: added basic structure [Task]: tutorial-part-3
    [Description list]
    feat: added basic structure
    
    [Notes]
    <notes>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
 
+const HERO = {
+  name: "Mariia",
+  age: "**",
+  ph: "(+380)987777777",
+  hobby: "tenis",
+}
+
 function App() {
 
   return (
     <div className="App">
-      <h3>This is a starting point</h3>
+      <header>
+        <h2>My life</h2>
+      </header>
+      <main>
+        <article>
+          <p> My name is {HERO.name}. I like {HERO.hobby}.</p>
+        </article>
+      </main>
+      <footer>
+        <ul>Contacts:
+          <li>{HERO.ph}</li>
+        </ul>  
+      </footer>
+
     </div>
   )
 }
