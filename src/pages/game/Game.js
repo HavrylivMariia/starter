f47bf6d0372ca318e491829a2936740050960a09
mariia.havryliv <mariia.havryliv@gmail.com>
2022-12-21 21:10:01 +0200

    chore: function added [Task]: tutorial-part-4
    [Descriptions list]
    feat: function added
    
    [Notes]
    <notes>

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -4,7 +4,22 @@ function toSquare(x) {
     return x * x 
 }
 
+function printHello(msg) {
+    console.log(`Hello ${msg}`);
+}
+
+function printHello2(msg) {
+    console.log("Hello " + msg);
+}
+
+function printHello3(msg) {
+    const resultMsg = "Hello" + " " + msg;
+    console.log(resultMsg);
+}
+
 let x = 4;
 let y = toSquare(x);
-console.log(y);
+printHello("Denys");
+printHello2("Denys");
+printHello3("Marichka");
  
